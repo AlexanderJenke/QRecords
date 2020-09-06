3466f5a5f0935b38c1357afc39e1a4118fa96210
FriwiDev <bblosaurus@web.de>
2020-09-06 18:39:05 +0200

    Adapt charsets

--- src/charsets.ts
+++ src/charsets.ts
@@ -1,3 +1,3 @@
-export const VARTEXT_CHARSET = "? 0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz.!-+_=><ÄÖÜäöüßáàâéeêìíîóòôúùûÁÀÂÉÈÊÍÌÎÓÒÔÚÙÛ";
-export const NAME_CHARSET = "? ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz-ÄÖÜäöüßáàâéeêìíîóòôúùûÁÀÂÉÈÊÍÌÎÓÒÔÚÙÛ";
-export const PHONE_CHARSET = "? 0123456789+";
+export const VARTEXT_CHARSET = "? 0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz.!-+_=><ÄÖÜäöüßáàâéeêìíîóòôúùûÁÀÂÉÈÊÍÌÎÓÒÔÚÙÛ\n";
+export const NAME_CHARSET = "? ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz-ÄÖÜäöüßáàâéeêìíîóòôúùûÁÀÂÉÈÊÍÌÎÓÒÔÚÙÛ.";
+export const PHONE_CHARSET = "? 0123456789+#";
